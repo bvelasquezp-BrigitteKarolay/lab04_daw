50fd502c9c2643db1d6f7957a249ab267ba36964
Valeria TN <vticonan@unsa.edu.pe>
2026-05-07 15:21:38 -0500

    Cambios

--- lab04/python_basic/01-holamundo/holamundo.py
+++
@@ -1,4 +0,0 @@
-print("###################")
-print("Hola mundo !! Soy Richart Escobedo el alumno del Master Python")
-print("###################")
-print(2021)
--- lab04/python_basic/04-entrada-salida/main.py
+++
@@ -1,6 +0,0 @@
-# Entrada
-nombre = input("¿Cual es tu nombre?:")
-edad = input("¿cual es tu edad?:")
-
-# Salida
-print(f"Me alegro de conocerte, bienvenido {nombre}!! veo que tienes {int(edad)+2} años!!")
